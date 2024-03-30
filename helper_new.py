fa004c31e4a4e7297943d6bc6890398f605060a7
adityarjha17 <jha.aditya17@gmail.com>
2024-03-30 09:23:55 +0530

    first commit

---
+++ helper_new.py
@@ -0,0 +1,48 @@
+from urlextract import URLExtract
+import pandas as pd
+from collections import Counter
+
+extract = URLExtract()
+
+
+def fetch_stats(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user] #slicing
+    num_messages = df.shape[0]
+    words = []
+    for message in df['message']:
+        words.extend(message.split())
+
+    num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0] #slicing
+
+    links = []
+    for message in df['message']:
+        links.extend(extract.find_urls(message))
+    return num_messages, len(words), num_media_messages, len(links)
+
+
+def most_busy_users(df):
+    x = df['user'].value_counts().head(10)
+    df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
+        columns={'index': 'name', 'user': 'percent'})
+    return x, df
+
+def most_common_words(selected_user, df):
+    f = open('stop_hinglish.txt', 'r')
+    stop_words = f.read()
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user] #slicing
+
+    temp = df[df['user'] != 'group_notification'] #slicing
+    temp = temp[temp['message'] != '<Media omitted>\n']
+
+    words = []
+
+    for message in temp['message']:
+        for word in message.lower().split():
+            if word not in stop_words:
+                words.append(word)
+
+    most_common_df = pd.DataFrame(Counter(words).most_common(20))
+    return most_common_df
